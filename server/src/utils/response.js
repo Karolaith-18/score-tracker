8694a2a202a6624aeb128c5ec5bcce2d2fd148fe
Karolaith Orozco <karolaithorozco16@gmail.com>
2026-05-20 12:26:23 -0500

    fix: add fail helper to response utils

--- server/src/utils/response.js
+++ server/src/utils/response.js
@@ -1,13 +1,8 @@
 /**
  * utils/response.js
  * Helpers para respuestas HTTP estandarizadas.
- * Todos los controllers usan estas funciones para mantener
- * un formato consistente en toda la API.
  */
 
-export const ok      = (res, data, status = 200) => res.status(status).json({ ok: true,  data })
-export const created = (res, data)               => res.status(201).json({ ok: true,  data })
-export const notFound = (res, msg = 'Not found') => res.status(404).json({ ok: false, message: msg })
-export const badRequest = (res, msg)             => res.status(400).json({ ok: false, message: msg })
-export const serverError = (res, msg = 'Internal server error') =>
-  res.status(500).json({ ok: false, message: msg })
+export const ok      = (res, data, status = 200) => res.status(status).json({ ok: true, data })
+export const fail    = (res, msg, status = 400)  => res.status(status).json({ ok: false, message: msg })
+export const created = (res, data)               => res.status(201).json({ ok: true, data })
