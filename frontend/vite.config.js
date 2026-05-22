0b0b94b026a148008c25c4fb7c2b9363d64d0abc
Karolaith Orozco <karolaithorozco16@gmail.com>
2026-05-21 22:38:19 -0500

    fix: disable proxy in production build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  server: {
+  server: mode === 'development' ? {
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
       }
     }
-  }
-})
+  } : {}
+}))
